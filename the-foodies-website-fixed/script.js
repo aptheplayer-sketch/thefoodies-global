1ff10009f415eca2275f4dfbd7612c25ca913043
aptheplayer-sketch <a.ptheplayer@gmail.com>
2026-02-20 04:20:03 +0530

    Add files via upload

---
+++ the-foodies-website-fixed/script.js
@@ -0,0 +1,10 @@
+function sendWhatsApp() {
+  var name = document.getElementById('name').value;
+  var city = document.getElementById('city').value;
+  var phone = document.getElementById('phone').value;
+
+  var msg = `New Partner Request:%0AName: ${name}%0ACity: ${city}%0APhone: ${phone}`;
+  var url = `https://wa.me/917357666696?text=${msg}`;
+
+  window.open(url, '_blank');
+}
