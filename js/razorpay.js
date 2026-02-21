8e2db1b6693162bde252de55bce7eef3ac2372ab
aptheplayer-sketch <a.ptheplayer@gmail.com>
2026-02-21 10:52:05 +0530

    Delete js directory

--- js/razorpay.js
+++
@@ -1,4 +0,0 @@
-
-function startPayment(amount, description){
- alert("Razorpay payment flow ready. Connect live key.");
-}
--- js/supabase.js
+++
@@ -1,4 +0,0 @@
-
-const SUPABASE_URL = "https://azcgvkoxlxmmtcfpgslx.supabase.co";
-const SUPABASE_KEY = "YOUR_PUBLIC_ANON_KEY";
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
